998085691fc960e294ab080cd2c969c8d05be382
Yen-Ru Lai <ycpss94012@gmail.com>
2025-03-29 20:02:34 +0800

    Update Homework 6 (add 2 unnecessary spaces).

--- src/hw6/Calculator.java
+++ src/hw6/Calculator.java
@@ -15,12 +15,12 @@ public class Calculator {
 			throw new ArithmeticException("次方為負值，結果回傳不為整數！");
 		}
 	}
-	public int powerXY() throws CalException{
+	public int powerXY() throws CalException {
 		if (x==0 && y==0) {
 			throw new CalException("0的0次方沒有意義！");
 		} else {
 			System.out.println(x + "的" + y + "次方等於" + (int)Math.pow(x, y) + "。");
-			return (int) Math.pow(x, y);
+			return (int)Math.pow(x, y);
 		}
 	}
 }
